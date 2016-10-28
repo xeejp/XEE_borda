393263fe8cd996f1d3ba8e24ebca3c649ffd200b
kazuwo <623taiki@gmail.com>
2016-10-28 15:47:41 +0900

    make-cvs-download is complete

--- host/DownloadButton.js
+++ host/DownloadButton.js
@@ -1,36 +1,48 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import FileFileDownload from 'material-ui/svg-icons/file/file-download'
 
-const mapStateToProps = ({ participants, answered }) => ({
-  participants, answered
+const mapStateToProps = ({ participants }) => ({
+  participants
 })
-class DownloadButton extends Component {
 
-  render() {
-    const { participants,  style, disabled, answered } = this.props
-    var content 
-      = "ID,1問目の回答,2問目の回答\n"
+const DownloadButton = ({ participants, style, disabled }) => (
+  <FloatingActionButton
+    style={style}
+    disabled={disabled}
+    onClick={() => {
+      var fileName = "borda_result.csv"
+
+      var content 
+        = "ボルダルール実験\n" 
+        + "実験日," + new Date() + "\n"
+        + "登録者数," + Object.keys(participants).length + "\n"
+        + "ID,5段階評価:pa,pb,pc,qa,qb,qc,並び替え:pの1位,pの2位,pの3位,qの1位,qの2位,qの3位,評価軸の1位,評価軸の2位\n"
         + Object.keys(participants).map(id => [id, 
-            
-            "5段階評価",
 
             participants[id].question1,
-            
-            "Drag and Drop",
-
             participants[id].question2
-        
+
         ].join(',')).join("\n")
-    return (
-      <div>
-      <p>{content}</p>
-      <p>answered:{answered}</p>
-    </div>
-    )
-  }
-}
+
+      var blob = new Blob([content]);
+      var url = window.URL || window.webkitURL;
+      var blobURL = url.createObjectURL(blob);
+
+      if(window.navigator.msSaveBlob){
+        window.navigator.msSaveBlob(blob, fileName)
+      }
+      else{
+        var a = document.createElement('a');
+        a.download = fileName;
+        a.href = blobURL;
+        a.click();  
+      }
+    }
+    }
+  ><FileFileDownload /></FloatingActionButton>
+)
 
 export default connect(mapStateToProps)(DownloadButton) 
